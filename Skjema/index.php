16d6fafb684e0dd9906b7dab38648433b6cd9a2b
isakbh <isak@brunhenriksen.net>
2025-01-06 11:08:03 +0100

    Lagt til skjema greier

---
+++ Skjema/index.php
@@ -0,0 +1,20 @@
+<iframe id="forms" src="https://docs.google.com/forms/d/e/1FAIpQLSeTIhhyRBpJ04otG1If2vJyVrX5yV4IzMJEKFTVRxM45LbXuQ/viewform?usp=header"
+        frameborder="0" marginheight="0"
+        marginwidth="0">
+    </iframe>
+
+    <style>
+        #forms {
+            position: absolute;
+            transform: translate(-50%, -50%);
+            left: 50%;
+            top: 50%;
+            width: 95%;
+            height: 95%;
+            border-radius: 20px;
+            text-decoration: none;
+            filter: sepia(30%);
+            border: 2px solid black;
+            overflow: hidden;
+        }
+    </style>
