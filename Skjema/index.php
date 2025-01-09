f9fe362ae169670a3b20d435067209427a890bdd
isakbh <isak@brunhenriksen.net>
2025-01-09 09:51:59 +0100

    Fikset skjema siden

--- Skjema/index.php
+++ Skjema/index.php
@@ -2,10 +2,7 @@
 <div id="mainContent">
         <h1>Skjema om avfall</h1>
 
-        <iframe id="forms" src="https://docs.google.com/forms/d/e/1FAIpQLSeTIhhyRBpJ04otG1If2vJyVrX5yV4IzMJEKFTVRxM45LbXuQ/viewform?usp=header"
-                frameborder="0" marginheight="0"
-                marginwidth="0">
-        </iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeTIhhyRBpJ04otG1If2vJyVrX5yV4IzMJEKFTVRxM45LbXuQ/viewform?embedded=true" width="640" height="2532" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
 
 
@@ -16,23 +13,15 @@
             text-decoration: none;
             filter: sepia(30%);
             border: 2px solid black;
-            overflow: hidden;
         }
 
         #mainContent {
             background-color: aliceblue;
-            justify-content: center !important;
-            text-align: center;
-            margin: 10px 10px;
             border-radius: 20px;
-            width: 50%;
             padding: 10px;
-            overflow: hidden;
-            max-height: 1000px;
+            height: auto;
             border: 2px solid rgba(144, 203, 255, 0.5);
-            position: absolute;
-            transform: translate(-50%, -80%);
-            left: 50%;
-            top: 50%;
+            text-align: center;
+
         }
     </style>
