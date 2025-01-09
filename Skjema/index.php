2b6d89992c10035de8b1d8a3c8f4d9b3b84ad3a3
isakbh <isak@brunhenriksen.net>
2025-01-09 10:14:08 +0100

    Jobber fortsatt med skjema, har gjort den fin nå

--- Skjema/index.php
+++ Skjema/index.php
@@ -1,27 +1,16 @@
+<html>
 
-<div id="mainContent">
+<head>
+<link rel="stylesheet" href="skjema.css">
+</head>
+
+<body>
+
+    <div id="mainContent">
         <h1>Skjema om avfall</h1>
-
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeTIhhyRBpJ04otG1If2vJyVrX5yV4IzMJEKFTVRxM45LbXuQ/viewform?embedded=true" width="640" height="2532" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
 
+</body>
 
-    <style>
-        #forms {
-            height: 50%;
-            width: 80%;
-            text-decoration: none;
-            filter: sepia(30%);
-            border: 2px solid black;
-        }
-
-        #mainContent {
-            background-color: aliceblue;
-            border-radius: 20px;
-            padding: 10px;
-            height: auto;
-            border: 2px solid rgba(144, 203, 255, 0.5);
-            text-align: center;
-
-        }
-    </style>
+</html>
