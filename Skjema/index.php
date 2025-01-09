55441608fd783c9f2e5d6aeb004a70e0b8bcb750
isakbh <isak@brunhenriksen.net>
2025-01-09 10:50:47 +0100

    Fikset dark mode av dropdown, bedre skjema

--- Skjema/index.php
+++ Skjema/index.php
@@ -1,10 +1,16 @@
 <html>
 
 <head>
-<link rel="stylesheet" href="skjema.css">
+    <link rel="stylesheet" href="skjema.css">
+    <link rel="icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkommunikasjon.ntb.no%2Fdata%2Fimages%2F00977%2Fd09cafe1-2b7d-46c2-8666-54390c651a52-w_960_h_960.jpg&f=1&nofb=1&ipt=f18de45e2706cf33e46d94df518c4aa207cd3536229c14afee8433c19fbeb28c&ipo=images">
+    <script src="../Javascript/toggle.js"></script>
 </head>
 
 <body>
+    <!--- Navigasjonsbar -->
+    <?php
+    include("../Include/HTML/navbar.html");
+    ?>
 
     <div id="mainContent">
         <h1>Skjema om avfall</h1>
