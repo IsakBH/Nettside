0fcd93df64cf9ac2d92d1f39eb603035b98272aa
IsakBH <89648535+IsakBH@users.noreply.github.com>
2025-01-26 18:54:57 +0100

    Den originale indexen
    Gjorde en feil med å overskrive indeksen med naturfagsprosjekt indeksen

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="no">
+
+<head>
+    <title> Isak B. Henriksen </title>
+    <link rel="icon" href="../Pictures/Tux.svg.png">
+    <link rel="stylesheet" href="../Stylesheets/styling2.css">
+    <script src="../Javascript/toggle.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+
+    <!-- Open Graph meta-tagger -->
+    <meta property="og:title" content="Isak Henriksen">
+    <meta property="og:description" content="Informasjonsteknologi og Medieproduksjon elev på Amalieskram VGs, Linux bruker.">
+    <meta property="og:image" content="https://isak.brunhenriksen.no/Pictures/selfie.jpg">
+    <meta property="og:url" content="https://isak.brunhenriksen.no">
+    <meta property="og:type" content="website">
+</head>
+
+<body>
+
+    <!--- Navigasjonsbar -->
+    <?php
+    include("Include/HTML/navbar.html");
+    ?>
+
+    <h1> Isak B. Henriksen </h1>
+
+<?php
+include("Logg/Text/ommeg.txt");
+?>
+
+    <button onclick="location.href = '/Undersider/side2.php';">Leke side</button>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<br>
+
+<div id="embedwrapper">
+    <iframe id="maps" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d150.92522395422816!2d5.233279111964471!3d60.3660681255376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sno!4v1734516022151!5m2!1sen!2sno" style="border:0;" allowfullscreen="yes" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+</body>
+
+</html>
