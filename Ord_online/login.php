a94760ad55cc551e650bb5d06dc4d9c9f006c6a5
IsakBH <isak@brunhenriksen.net>
2025-02-14 23:14:54 +0100

    Mobilvennlig login og registrerings sider

--- Ord_online/login.php
+++ Ord_online/login.php
@@ -34,6 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="texteditor.css">
     <script src="texteditor.js"></script>
     <link rel="icon" href="../Pictures/ordlogo.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </head>
 <body>
     <div class="auth-container">
