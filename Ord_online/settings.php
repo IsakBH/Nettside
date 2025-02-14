beec93e8fbd6dbee126f588a5f7f1c0ed674fa65
IsakBH <isak@brunhenriksen.net>
2025-02-14 16:05:58 +0100

    Mobilvennlig instillinger side

--- Ord_online/settings.php
+++ Ord_online/settings.php
@@ -87,6 +87,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Instillinger Ord på Nett</title>
     <link rel="stylesheet" href="texteditor.css">
     <script src="texteditor.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </head>
 
 <body>
