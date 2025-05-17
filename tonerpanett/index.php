6050c4f0c5c6e0b9628451c2a976bcd221904fec
isakbh <isak@brunhenriksen.net>
2025-05-17 19:15:32 +0200

    A new beginning - Toner på Nett

---
+++ tonerpanett/index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+
+<html>
+
+<body>
+<p>Dette blir hjemmet til den fremtidige Toner på Nett</p>
+
+<style>
+p {
+    justify-content: center;
+    text-align: center;
+}
+</style>
+
+</body>
+
+</html>
