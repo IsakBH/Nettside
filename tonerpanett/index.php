b2c0209b2e74d594fc05691d68f70cc8ff4979ee
isakbh <isak@brunhenriksen.net>
2025-05-17 20:52:59 +0200

    Lagt til styling og song manager (document manager fra ord på nett)

--- tonerpanett/index.php
+++ tonerpanett/index.php
@@ -9,13 +9,34 @@ $version = "v0.0.1";
 <head>
     <title>Toner på Nett <?php echo $version; ?></title>
 
+    <!-- meta tags for søkemotoroptimalisering -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- stylesheets -->
     <link rel="stylesheet" href="styling/main.scss" />
+
+    <!-- logoet til toner på nett som favicon -->
     <link rel="icon" href="assets/icons/logo.png" />
+
+    <!-- ikoner fra font awesome og google fonts-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
 </head>
 
 <body>
-
+    <div class="song-manager">
+        <div class="song-list">
+            <h3>Mine dokumenter</h3>
+            <button id="newSong" class="new-song-button">
+                <i class="fa-solid fa-plus"></i>
+                Ny sang
+            </button>
+            <div class="search-container">
+                <i class="fa-solid fa-search"></i>
+                <input type="text" id="songSearch" placeholder="Søk i sanger...">
+            </div>
+            <ul id="songsList"></ul>
+        </div>
+    </div>
 </body>
 
 </html>
