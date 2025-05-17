edb8bc281a080ae89af98f61d50f28aa2e5f3e36
isakbh <isak@brunhenriksen.net>
2025-05-17 20:11:42 +0200

    Laget stylesheet og fikset <title>

--- tonerpanett/index.php
+++ tonerpanett/index.php
@@ -7,23 +7,15 @@ $version = "v0.0.1";
 <html lang="en">
 
 <head>
-    <title>Ord På Nett <?php echo $version; ?></title>
+    <title>Toner på Nett <?php echo $version; ?></title>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="texteditor.css" />
+    <link rel="stylesheet" href="styling/main.scss" />
     <link rel="icon" href="assets/icons/logo.png" />
-    <!-- ikoner fra font awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
 </head>
 
 <body>
-    <p>Dette blir hjemmet til den fremtidige Toner på Nett</p>
 
-    <style>
-        p {
-            justify-content: center;
-            text-align: center;
-        }
-    </style>
 </body>
 
 </html>
