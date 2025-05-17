bc993e8d57b633c928f0c0683f64bbbff678c2e9
isakbh <isak@brunhenriksen.net>
2025-05-17 20:00:20 +0200

    Laget logo for Toner på Nett

--- tonerpanett/index.php
+++ tonerpanett/index.php
@@ -1,19 +1,29 @@
 <?php
-session_start();
+// variabel for versjonsnummer
+$version = "v0.0.1";
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Ord På Nett <?php echo $version; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="texteditor.css" />
+    <link rel="icon" href="assets/icons/logo.png" />
+    <!-- ikoner fra font awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+</head>
 
 <body>
-<p>Dette blir hjemmet til den fremtidige Toner på Nett</p>
+    <p>Dette blir hjemmet til den fremtidige Toner på Nett</p>
 
-<style>
-p {
-    justify-content: center;
-    text-align: center;
-}
-</style>
-
+    <style>
+        p {
+            justify-content: center;
+            text-align: center;
+        }
+    </style>
 </body>
 
 </html>
