71781f7a5018b7b5fcc70207c89d9beaa60a2b54
isakbh <isak@brunhenriksen.net>
2025-11-15 17:00:51 +0100

    bruker ordlogo.png istedet for opnlogo-highres-scaled

--- altpanett/index.php
+++ altpanett/index.php
@@ -7,7 +7,7 @@
 
     <body>
         <div class="sidebar">
-            <div class="sidebar-application-icon" data-url="/ordpanett/index.php"><img src="/ordpanett/assets/opnlogo-highres-scaled.png"></div>
+            <div class="sidebar-application-icon" data-url="/ordpanett/index.php"><img src="/ordpanett/assets/ordlogo.png"></div>
             <div class="sidebar-application-icon" data-url="/samtalerpanett/main.php"><img src="/samtalerpanett/assets/icons/logo.ico"></div>
             <div class="sidebar-application-icon" data-url="/tonerpanett/index.php"><img src="/tonerpanett/assets/icons/logo.png"></div>
         </div>
