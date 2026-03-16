63e793cbfd781b37b65d5872b0444576611f8efa
isakbh <isak@brunhenriksen.net>
2026-03-16 22:07:50 +0100

    lagt til info -- e-post, sted, linkedin profil, og github profil

--- portfolio/index.php
+++ portfolio/index.php
@@ -3,16 +3,22 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <link rel="stylesheet" href="styles/styling.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
     </head>
 
     <body>
         <div id="sidebar">
             <div id="bilde">
-                <img src="assets/PXL_20260314_135251334.jpg">
+                <img id="bilde-av-meg" src="assets/PXL_20260314_135251334.jpg">
             </div>
 
             <div id="info">
                 <h2>INFO</h2>
+                <hr>
+                <p><i class="fa-solid fa-envelope"></i> isak@brunheriksen.no</p>
+                <p><i class="fa-solid fa-compass"></i> Bergen, Norge</p>
+                <p><i class="fa-brands fa-square-linkedin"></i> <a href="https://linkedin.com/in/isakbh">in/isakbh</a></p>
+                <p><i class="fa-brands fa-square-github"></i> isakbh</p>
             </div>
         </div>
     </body>
