1c436503c08374c4f56c3cca10b1d8342771a1f8
isakbh <isak@brunhenriksen.net>
2026-03-16 21:44:34 +0100

    lagt til bilde i assets mappe og lagt bildet til i siden i index.php

--- portfolio/index.php
+++ portfolio/index.php
@@ -2,9 +2,12 @@
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="stylesheet" href="styles/styling.css">
     </head>
 
     <body>
-
+        <div id="sidebar">
+            <img src="assets/PXL_20260314_135251334.jpg">
+        </div>
     </body>
 </html>
